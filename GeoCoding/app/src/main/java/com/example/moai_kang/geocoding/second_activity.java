6775e8f69b7f34e5d8df0d19901eaf0859d44b56
moai_kang <ggw120703@gmail.com>
2019-08-08 16:57:05 +0900

    중간점검123

---
+++ GeoCoding/app/src/main/java/com/example/moai_kang/geocoding/second_activity.java
@@ -0,0 +1,25 @@
+package com.example.moai_kang.geocoding;
+
+import android.app.Activity;
+import android.location.Address;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import java.util.List;
+
+/**
+ * Created by Moai_Kang on 2019-08-05.
+ */
+
+public class second_activity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        List<Address> list = null;
+
+
+
+    }
+}
